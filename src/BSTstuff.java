ba7795add44a3424134bd13ad4e86cd8f737ce14
Harish Bommakanti <33380236+harishbommakanti@users.noreply.github.com>
2020-03-10 07:25:19 -0500

    Wrote methods for creating/inserting a new BST node
    - still needs to be tested

--- src/BSTstuff.java
+++ src/BSTstuff.java
@@ -1,2 +1,52 @@
 public class BSTstuff {
+    public static void main(String[] args)
+    {
+        Node root = null;
+        insert(root,1);
+        insert(root,2);
+        insert(root,3);
+        insert(root,4);
+        insert(root,5);
+    }
+
+    //creates/returns a node with default left/right/parent pointers
+    public static Node createNewNode(int val)
+    {
+        Node n = new Node();
+        n.key = val;
+        n.parent = null;
+        n.left = null;
+        n.right = null;
+        return n;
+    }
+
+    //inserts a node with the given value to the tree
+    public static void insert(Node root, int val)
+    {
+        Node newNode = createNewNode(val);
+        Node tempParent=null;
+        Node x=root;
+        while (x != null) //while the left or right subtree is not null
+        {
+            tempParent=x; //set the parent node to that x
+            if (val<x.key) //set x to left or right subtree, depending on which side key needs to go
+                x=x.left;
+            else
+                x=x.right;
+        }
+
+        newNode.parent=tempParent;
+        if (tempParent == null) //the tree was empty
+            root=newNode;
+        else if (val<tempParent.key) //val needs to go on left
+            tempParent.left=newNode;
+        else //val needs to go on right
+            tempParent.right=newNode;
+    }
+}
+
+class Node
+{
+    int key;
+    Node left,right,parent;
 }
