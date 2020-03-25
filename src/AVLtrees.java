3095a4f472bd6feaec812e734fc05c13bad4a4b0
Harish Bommakanti <33380236+harishbommakanti@users.noreply.github.com>
2020-03-25 12:48:29 -0500

    created AVLtrees.java

---
+++ src/AVLtrees.java
@@ -0,0 +1,13 @@
+/*AVL trees are an effort to make more balanced binary trees: the max difference between the height (max length from that
+node to a leaf) between the left and right children is +- 1. height(node) = height(left)
+*/
+
+public class AVLtrees
+{
+    private Node root;
+
+    public static void main(String[] args)
+    {
+        
+    }
+}
