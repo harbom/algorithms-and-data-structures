76e3fd79c8f06bfbd20809c3057192d6f37a20ee
Harish Bommakanti <33380236+harishbommakanti@users.noreply.github.com>
2020-03-25 14:22:27 -0500

    Added the CLRSRightRotate method, worked properly under tests

--- src/AVLtrees.java
+++ src/AVLtrees.java
@@ -22,7 +22,8 @@ public class AVLtrees
     {
         AVLtrees tree = new AVLtrees();
 
-        testLeftRotate();
+        //to test the left and right rotate functions
+        testRotate();
     }
 
     //returns the height (max len of the path from that node to a leaf) of the node
@@ -60,7 +61,36 @@ public class AVLtrees
         x.parent=y;
     }
 
-    private static void testLeftRotate()
+    //basically, same code as above function but switched the .left's and .right's
+    private static void CLRSRightRotate(AVLNode x)
+    {
+
+        /*for example, use this graphic:
+              y                *x*
+         *x*     r  <--     a       y
+        a   b                     b   r
+        */
+
+        AVLNode y = x.left; //y is x's left subtree (in pic above, its b)
+        //assign y's right subtree to x.left
+        x.left = y.right;
+
+        if (y.right != null) //if b.left is not null aka it has children
+            y.right.parent = x; //set y.left.parent (y itself) to x
+
+        y.parent = x.parent; //link x's parent to y.parent aka x.right now has x.parent as its parent, instead of x
+        if (x.parent == null) //if the parent is null, y is now null (y.p = null too so it works)
+            root=y;
+        else if (x == x.parent.left) //if x is a left child
+            x.parent.left = y;
+        else
+            x.parent.right = y;
+
+        y.right=x;
+        x.parent=y;
+    }
+
+    private static void testRotate()
     {
         //test for the left rotate feature
         /*
@@ -87,7 +117,8 @@ public class AVLtrees
 
         System.out.println("before: ");
         inorderTransversal(root);
-        CLRSleftRotate(two);
+        //CLRSleftRotate(two);
+        CLRSRightRotate(two);
         System.out.println("after: ");
         inorderTransversal(root);
         //it worked for this example, the inorder property was preserved
