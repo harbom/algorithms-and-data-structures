7c84255602c4451e8498f46c734e7f651448623c
Harish Bommakanti <33380236+harishbommakanti@users.noreply.github.com>
2020-04-14 18:54:24 -0500

    redid structure of class (made runner class, library class structure), added method to initialize a list of objects

--- src/Hashing.java
+++ src/Hashing.java
@@ -1,10 +1,45 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Hashing
 {
+    public static void main(String[] args)
+    {
+        HashingLibrary tester = new HashingLibrary();
+        tester.initializeObjectList();
+    }
+}
 
+class HashingLibrary
+{
+    private int maxIndex = 50;
+    private int numObjects = 10;
+    private List<exampleObject> objectList = new ArrayList<exampleObject>();
+
+    public void initializeObjectList()
+    {
+        for (int i = 0; i < numObjects; i++)
+            objectList.add(new exampleObject((int)(maxIndex*Math.random())));
+
+        confirmObjList();
+    }
+
+    private void confirmObjList()
+    {
+        System.out.println("confirmation of object list:");
+        for (exampleObject i:objectList)
+            System.out.println(i + " key:" + i.key);
+    }
 }
 
 class exampleObject
 {
     public int key;
     public Object data;
+
+    public exampleObject(int key)
+    {
+        this.key = key;
+    }
 }
