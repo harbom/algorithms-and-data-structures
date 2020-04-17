5e7bae5396dfb56178ff55cd1cb7b875fea2bf77
Harish Bommakanti <33380236+harishbommakanti@users.noreply.github.com>
2020-04-17 10:37:59 -0500

    added direct address table code for hashing

--- src/Hashing.java
+++ src/Hashing.java
@@ -8,6 +8,7 @@ public class Hashing
     {
         HashingLibrary tester = new HashingLibrary();
         tester.initializeObjectList();
+        tester.initializeDirectAddressTable();
     }
 }
 
@@ -15,7 +16,8 @@ class HashingLibrary
 {
     private int maxIndex = 50;
     private int numObjects = 10;
-    private List<exampleObject> objectList = new ArrayList<exampleObject>();
+    private List<exampleObject> objectList = new ArrayList();
+    private exampleObject[] arr = new exampleObject[maxIndex];
 
     public void initializeObjectList()
     {
@@ -25,11 +27,38 @@ class HashingLibrary
         confirmObjList();
     }
 
+    //builds a list of objects, as a prestage to direct address table or hashtable
     private void confirmObjList()
     {
         System.out.println("confirmation of object list:");
         for (exampleObject i:objectList)
             System.out.println(i + " key:" + i.key);
+    }
+
+    //adds all elements in list into an array so that search will be O(1)
+    //this doesn't solve collisions so the most recent duplicates will only be stored
+    public void initializeDirectAddressTable()
+    {
+        for (exampleObject i: objectList)
+        {
+            arr[i.key] = i;
+        }
+
+        confirmDirectAddressTable();
+    }
+
+    private void confirmDirectAddressTable()
+    {
+        System.out.println("Confirmation of direct address table");
+        for (int i=0; i<arr.length; i++)
+        {
+            System.out.print("index: " + i + " ");
+            if (arr[i] == null)
+                System.out.print("no object here");
+            else
+                System.out.print("object key: " + arr[i].key + "\tobject: " + arr[i]);
+            System.out.println();
+        }
     }
 }
 
